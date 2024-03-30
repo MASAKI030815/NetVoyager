6bd9d35e8d3f19d52ac298c64b086b136c905625
Ginjo0815 <my.g.0815@gmail.com>
2024-03-31 00:05:33 +0900

    +

--- script.py
+++ script.py
@@ -90,18 +90,18 @@ def ping_gateway_v4():
     gateways = netifaces.gateways()
     default_gateway = gateways['default'][netifaces.AF_INET][0]
     
-    # ラージパケットでの疎通確認
-    large_packet_cmd = ["ping"] + pingv4_large_option + [default_gateway]
-    large_packet_result = subprocess.run(large_packet_cmd, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
-    large_color = "\033[92m" if large_packet_result.returncode == 0 else "\033[91m"
-    large_status = "OK" if large_packet_result.returncode == 0 else "NG"
-    
     # ショートパケットでの疎通確認
     short_packet_cmd = ["ping"] + pingv4_short_option + [default_gateway]
     short_packet_result = subprocess.run(short_packet_cmd, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
     short_color = "\033[92m" if short_packet_result.returncode == 0 else "\033[91m"
     short_status = "OK" if short_packet_result.returncode == 0 else "NG"
 
+    # ラージパケットでの疎通確認
+    large_packet_cmd = ["ping"] + pingv4_large_option + [default_gateway]
+    large_packet_result = subprocess.run(large_packet_cmd, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+    large_color = "\033[92m" if large_packet_result.returncode == 0 else "\033[91m"
+    large_status = "OK" if large_packet_result.returncode == 0 else "NG"
+    
     # 結果が両方OKまたはNGの場合の処理
     if short_status == "OK" and large_status == "OK":
         status_symbol = "\033[92mOK\033[0m"
