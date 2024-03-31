9c059fc7b3743afe5d584d60862019c20a41990e
Ginjo0815 <my.g.0815@gmail.com>
2024-03-31 15:56:48 +0900

    +

--- script.py
+++ script.py
@@ -60,8 +60,6 @@ response_virus_checks_lock = threading.Lock()
 response_mtr_checks = []
 response_mtr_checks_lock = threading.Lock()
 
-
-
 def myipaddr():
     global interface
     ipv6_addr = None
@@ -70,20 +68,17 @@ def myipaddr():
     gateway = None
     try:
         addrs = netifaces.ifaddresses(interface)
-        # IPv4アドレスとネットマスクの取得
         if netifaces.AF_INET in addrs:
             ipv4_info = addrs[netifaces.AF_INET][0]
             ipv4_addr = ipv4_info.get('addr')
             netmask = ipv4_info.get('netmask')
-        # デフォルトゲートウェイの取得
         gateways = netifaces.gateways()
         if netifaces.AF_INET in gateways['default']:
             gateway = gateways['default'][netifaces.AF_INET][0]
-        # IPv6アドレスの取得（リンクローカルアドレスを除外）
         if netifaces.AF_INET6 in addrs:
             for addr_info in addrs[netifaces.AF_INET6]:
                 if addr_info['addr'].startswith('fe80') is False:
-                    ipv6_addr = addr_info['addr'].split('%')[0]  # ゾーンインデックスを除去
+                    ipv6_addr = addr_info['addr'].split('%')[0]
                     break
     except Exception as e:
         print(f"IPアドレス取得中にエラーが発生しました: {e}")
@@ -95,68 +90,56 @@ def ping_gateway_v4():
     gateways = netifaces.gateways()
     default_gateway = gateways['default'][netifaces.AF_INET][0]
     
-    # ショートパケットでの疎通確認
     short_packet_cmd = ["ping"] + pingv4_short_option + [default_gateway]
     short_packet_result = subprocess.run(short_packet_cmd, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
     short_status = "OK" if short_packet_result.returncode == 0 else "NG"
     short_color = "\033[92m" if short_status == "OK" else "\033[91m"
 
-    # ラージパケットでの疎通確認
     large_packet_cmd = ["ping"] + pingv4_large_option + [default_gateway]
     large_packet_result = subprocess.run(large_packet_cmd, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
     large_status = "OK" if large_packet_result.returncode == 0 else "NG"
     large_color = "\033[92m" if large_status == "OK" else "\033[91m"
     
-    # 全体のステータスの決定
     status = "OK" if short_status == "OK" and large_status == "OK" else "NG"
     status_color = "\033[92m" if status == "OK" else "\033[91m"
 
-    # 結果の結合
     combined_status = f"{status_color}{status}\033[0m ({short_color}Short\033[0m / {large_color}Large\033[0m) : {default_gateway}"
 
     return combined_status
 
 def ping_internet_v4(host, name):
-    # ショートパケットでの疎通確認
     short_packet_cmd = ["ping"] + pingv4_short_option + [host]
     short_packet_result = subprocess.run(short_packet_cmd, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
     short_status = "OK" if short_packet_result.returncode == 0 else "NG"
     short_color = "\033[92m" if short_status == "OK" else "\033[91m"
 
-    # ラージパケットでの疎通確認
     large_packet_cmd = ["ping"] + pingv4_large_option + [host]
     large_packet_result = subprocess.run(large_packet_cmd, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
     large_status = "OK" if large_packet_result.returncode == 0 else "NG"
     large_color = "\033[92m" if large_status == "OK" else "\033[91m"
     
-    # 全体のステータスの決定
     status = "OK" if short_status == "OK" and large_status == "OK" else "NG"
     status_color = "\033[92m" if status == "OK" else "\033[91m"
 
-    # 結果の結合
     combined_status = f"{status_color}{status}\033[0m ({short_color}Short\033[0m / {large_color}Large\033[0m) : {host} ({name})"
     
     with response_ping_internet_v4_lock:
         response_ping_internet_v4.append(combined_status)
 
 def ping_internet_v6(host, name):
-    # ショートパケットでの疎通確認
     short_packet_cmd = ["ping6"] + pingv6_short_option + [host]
     short_packet_result = subprocess.run(short_packet_cmd, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
     short_status = "OK" if short_packet_result.returncode == 0 else "NG"
     short_color = "\033[92m" if short_status == "OK" else "\033[91m"
 
-    # ラージパケットでの疎通確認
     large_packet_cmd = ["ping6"] + pingv6_large_option + [host]
     large_packet_result = subprocess.run(large_packet_cmd, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
     large_status = "OK" if large_packet_result.returncode == 0 else "NG"
     large_color = "\033[92m" if large_status == "OK" else "\033[91m"
     
-    # 全体のステータスの決定
     status = "OK" if short_status == "OK" and large_status == "OK" else "NG"
     status_color = "\033[92m" if status == "OK" else "\033[91m"
 
-    # 結果の結合
     combined_status = f"{status_color}{status}\033[0m ({short_color}Short\033[0m / {large_color}Large\033[0m) : {host} ({name})"
     
     with response_ping_internet_v6_lock:
@@ -190,7 +173,6 @@ def check_http_response(url, name):
     with response_http_checks_lock:
         response_http_checks.append(status)
 
-# ウイルスチェックを行う関数
 def check_virus_download(url, name):
     try:
         local_filename = url.split('/')[-1]
@@ -205,7 +187,6 @@ def check_virus_download(url, name):
     with response_virus_checks_lock:
         response_virus_checks.append(status)
 
-# HTTPチェックを行うスレッド関数
 def threading_http_checks():
     threads = []
     for target in http_check_targets:
@@ -215,7 +196,6 @@ def threading_http_checks():
     for thread in threads:
         thread.join()
 
-# ウイルスチェックを行うスレッド関数
 def threading_virus_checks():
     threads = []
     for target in virus_check_targets:
@@ -233,11 +213,9 @@ def check_mtr(target, name, version='ipv4'):
 
     try:
         result = subprocess.run(mtr_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        
-        # 結果のテキストからIPアドレスを探し、対応する緑色の文字列に置換
         highlighted_result = result.stdout
         for ip_address, replacement in (mtr_v4_mark_hosts + mtr_v6_mark_hosts):
-            highlighted_replacement = f"\033[92m{replacement}\033[0m"  # 緑色にする
+            highlighted_replacement = f"\033[92m{replacement}\033[0m"
             highlighted_result = re.sub(r'\b{}\b'.format(re.escape(ip_address)), highlighted_replacement, highlighted_result)
 
         output = f"{name} ({target}) - IPv{version[-1]}:\n{highlighted_result}"
@@ -247,21 +225,16 @@ def check_mtr(target, name, version='ipv4'):
     with response_mtr_checks_lock:
         response_mtr_checks.append(output)
 
-
-
 def threading_mtr_checks():
     threads = []
-    # IPv4のターゲットに対するMTRチェックのスレッドを作成
     for target in mtr_v4_targets:
         thread = threading.Thread(target=check_mtr, args=(target[0], target[1], 'ipv4'))
         threads.append(thread)
         thread.start()
-    # IPv6のターゲットに対するMTRチェックのスレッドを作成
     for target in mtr_v6_targets:
         thread = threading.Thread(target=check_mtr, args=(target[0], target[1], 'ipv6'))
         threads.append(thread)
         thread.start()
-    # すべてのスレッドが終了するまで待機
     for thread in threads:
         thread.join()
 
