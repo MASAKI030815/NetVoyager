8766d6aa45306e011296d64a08e083e6c2aac0ec
Ginjo0815 <my.g.0815@gmail.com>
2024-03-31 22:28:18 +0900

    +

--- script.py
+++ script.py
@@ -14,8 +14,8 @@ pingv4_targets = [
     ["8.8.4.4", "Google DNS Backup"],
 ]
 pingv6_targets = [
-    ["2606:4700:4700::1111", "Cloudflare DNS IPv6"],
-    ["2606:4700:4700::1001", "Cloudflare DNS Backup IPv6"],
+    ["2001:4860:4860::8888", "Google DNS IPv6"],
+    ["2001:4860:4860::8844", "Google DNS Backup IPv6"],
 ]
 pingv4_large_option = ["-c", "1", "-M", "do", "-s", "1472", "-W", "1"]
 pingv4_short_option = ["-c", "1", "-s", "64", "-W", "1"]
@@ -23,23 +23,23 @@ pingv6_large_option = ["-c", "1", "-s", "1000", "-W", "1"]
 pingv6_short_option = ["-c", "1", "-s", "128", "-W", "1"]
 http_check_targets = [
     ["http://ipv4.google.com", "Google-IPv4"],
-    ["http://ipv6.google.com", "Google-IPv6"],
+    ["http://ipv6test.google.com/", "Google-IPv6"],
 ]
 virus_check_targets = [
     ["http://example.com/malicious_file", "Malicious File 1"],
     ["http://example.org/bad_file", "Malicious File 2"]
 ]
 mtr_v4_targets = [
-    ["139.130.4.5", "Australia DNS"],
+    ["google.com", "Google DNS"],
 ]
 mtr_v6_targets = [
-    ["2001:4860:4860::8888", "Google DNS IPv6"],
+    ["ipv6test.google.com", "Google DNS IPv6"],
 ]
 mtr_v4_mark_hosts = [
-    ["139.130.4.5", "Australia DNS"],
+    ["google.com", "Google DNS"],
 ]
 mtr_v6_mark_hosts = [
-    ["2001:4860:4860::8888", "Google DNS IPv6"],
+    ["ipv6test.google.com", "Google DNS IPv6"],
 ]
 #-----------------------
 
