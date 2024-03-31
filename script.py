03e08b706ff396602b293cbdf405d9a8816eebaf
Ginjo0815 <my.g.0815@gmail.com>
2024-03-31 22:41:51 +0900

    +

--- script.py
+++ script.py
@@ -294,7 +294,7 @@ def update_cli():
     if ipv6_addr:
         print(f"IPv6 Address: {ipv6_addr}")
 
-    print(f"\033[1m\033[93m\n-------Gateway Ping Result-------\033[0m")
+    print("\033[1m\033[93m\n-------Gateway Ping Result-------\033[0m")
     print(response_ping_gateway_v4)
 
     print("\033[1m\033[93m\n-------IPv4 Ping Results-------\033[0m")
@@ -313,9 +313,15 @@ def update_cli():
     for status in response_virus_checks:
         print(status)
 
-    print("\033[1m\033[93m\n-------MTR Results-------\033[0m")
+    print("\033[1m\033[93m\n-------IPv4 MTR Results-------\033[0m")
     for result in response_mtr_checks:
-        print(result)
+        if 'IPv4' in result:
+            print(result)
+
+    print("\033[1m\033[93m\n-------IPv6 MTR Results-------\033[0m")
+    for result in response_mtr_checks:
+        if 'IPv6' in result:
+            print(result)
     
 if __name__ == '__main__':       
     while True:
