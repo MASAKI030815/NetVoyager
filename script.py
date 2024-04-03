27b3c068a64007e13e7c8ad2ec1e2424bf51ca20
AKIRA <my.g.0815@gmail.com>
2024-04-03 15:18:27 +0900

    +

--- script.py
+++ script.py
@@ -18,8 +18,8 @@ pingv6_targets = [
 ]
 pingv4_large_option = ["-c", "1", "-M", "do", "-s", "1472", "-W", "1"]
 pingv4_short_option = ["-c", "1", "-s", "1472", "-W", "1"]
-pingv6_large_option = ["-c", "1", "-s", "1300", "-W", "1"]
-pingv6_short_option = ["-c", "1", "-s", "128", "-W", "1"]
+pingv6_large_option = ["-c", "2", "-s", "1300", "-W", "1"]
+pingv6_short_option = ["-c", "2", "-s", "128", "-W", "1"]
 http_check_targets = [
     ["http://ipv4.google.com", "Google-IPv4"],
     ["http://ipv6test.google.com/", "Google-IPv6"],
