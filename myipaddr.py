864385079b7bad7482fc0b30a017fd0285a816a7
AKIRA <my.g.0815@gmail.com>
2024-04-03 16:12:16 +0900

    +

--- myipaddr.py
+++ myipaddr.py
@@ -24,12 +24,11 @@ def myipaddr():
         ipv6_addr = ipv6_match.group(1) if ipv6_match else None
 
         # ゲートウェイの取得
-        gw_cmd = "route"
+        gw_cmd = "ip route show default"
         gw_result = subprocess.check_output(gw_cmd.split()).decode('utf-8')
-        gw_pattern = rf'default (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
+        gw_pattern = r'default via (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
         gw_match = re.search(gw_pattern, gw_result)
         gateway = gw_match.group(1) if gw_match else None
-        
         return ipv4_addr, netmask, gateway, ipv6_addr
     except Exception as e:
         print(f"IPアドレス取得中にエラーが発生しました: {e}")
