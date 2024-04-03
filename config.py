048f6a21da162aaa33d68cfbe3675c6ec31df574
AKIRA <my.g.0815@gmail.com>
2024-04-03 18:18:37 +0900

    +

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ pingv4_targets = [
     ["8.8.8.8", "Google DNS"],
     ["8.8.4.4", "Google DNS Backup"],
 ]
-gv6_targets = [
+pingv6_targets = [
 	["2001:4860:4860::8888", "Google DNS IPv6"],
     ["2001:4860:4860::8844", "Google DNS Backup IPv6"],
 ]
