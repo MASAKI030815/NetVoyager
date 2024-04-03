8ee23bfad9ee9b1871b64aa4ea4108ac39afd000
AKIRA <my.g.0815@gmail.com>
2024-04-03 15:20:46 +0900

    +

--- test.sh
+++ test.sh
@@ -2,6 +2,6 @@
 sudo rm -r NetVoyager
 git clone https://github.com/Ginjo0815/NetVoyager.git
 chmod +x ./NetVoyager/script.py
-nmcli c down eth0
-nmcli c up eth0
+sudo nmcli c down eth0
+sudo nmcli c up eth0
 python3 ./NetVoyager/script.py
