ed70b4638fb3444ecd3d913caa134b3f9a6b12dc
AKIRA <my.g.0815@gmail.com>
2024-04-05 16:36:33 +0900

    +

--- ping_internet_v4.py
+++ ping_internet_v4.py
@@ -2,9 +2,9 @@ import subprocess
 import config
 import threading
 
-
-response_ping_internet_v4 = []
-response_ping_internet_v4_lock = threading.Lock()
+response_ping_internet_v4 = ""
+#response_ping_internet_v4 = []
+#response_ping_internet_v4_lock = threading.Lock()
 
 def ping_internet_v4(host,name,pingv4_short_option,pingv4_large_option):
     short_packet_cmd = ["ping"] + pingv4_short_option + [host]
@@ -22,21 +22,22 @@ def ping_internet_v4(host,name,pingv4_short_option,pingv4_large_option):
 
     combined_status = f"{status_color}{status}\033[0m ({short_color}Short\033[0m / {large_color}Large\033[0m) : {host} ({name})"
     
-    with response_ping_internet_v4_lock:
-        response_ping_internet_v4.append(combined_status)
-        return response_ping_internet_v4
-    
-def theading_ping_internet_v4():
-    threads = []
-    for i in range(len(config.pingv4_targets)):
-        thread = threading.Thread(target=ping_internet_v4, args=(config.pingv4_targets[i][0], config.pingv4_targets[i][1]))
-        threads.append(thread)
-        thread.start()
-    for thread in threads:
-        thread.join()
-
-def threading_ping_v4():
-    thread = threading.Thread(target=theading_ping_internet_v4)
-    thread.start()
-    return thread
+    #with response_ping_internet_v4_lock:
+    #    response_ping_internet_v4.append(combined_status)
+    #    return response_ping_internet_v4
+    response_ping_internet_v4.append(combined_status)
+    return response_ping_internet_v4
+#def theading_ping_internet_v4():
+#    threads = []
+#    for i in range(len(config.pingv4_targets)):
+#        thread = threading.Thread(target=ping_internet_v4, args=(config.pingv4_targets[i][0], config.pingv4_targets[i][1]))
+#        threads.append(thread)
+#        thread.start()
+#    for thread in threads:
+#        thread.join()
+#
+#def threading_ping_v4():
+#    thread = threading.Thread(target=theading_ping_internet_v4)
+#    thread.start()
+#    return thread
 
